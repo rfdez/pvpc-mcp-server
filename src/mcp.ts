7d940c25db2fdaf4c4d0a3214712edcdafcbc57b
Raúl Fernández Fernández <49290264+rfdez@users.noreply.github.com>
2025-08-26 18:33:16 +0200

    fix: update mcp version

--- src/mcp.ts
+++ src/mcp.ts
@@ -13,7 +13,7 @@ export class PvpcMcpServer {
 			{
 				name: "pvpc-mcp-server",
 				title: "Voluntary Price for the Small Consumer (PVPC) MCP Server",
-				version: "3.1.0",
+				version: "3.2.0",
 			},
 			{
 				instructions:
